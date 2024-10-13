c95e9ac98cefd840020136fdafd57c9469a0c65b
agpirate <saytotsegay@gmail.com>
2024-10-13 10:22:42 -0400

    build 01

---
+++ client/assets/axios.9ae30fdc.js
@@ -0,0 +1,1 @@
+import{j as i}from"./index.39fdb616.js";import{a}from"./axios.05cf8750.js";const r=a.create({baseURL:"http://127.0.0.1:8080"});var e=i(({app:o})=>{o.config.globalProperties.$axios=a,o.config.globalProperties.$api=r});export{r as api,e as default};
---
+++ client/assets/use-quasar.0e73d800.js
@@ -0,0 +1,1 @@
+import{i as a,E as r}from"./index.39fdb616.js";function u(){return a(r)}export{u};
